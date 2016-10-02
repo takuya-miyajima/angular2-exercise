4c1771f33f4944e41d13477bdc4a4d66a09ff03b
Takuya Miyajima <miyajima@acroquest.co.jp>
2016-10-02 22:16:18 +0900

    テンプレートエラー対応 This reverts commit 719583114144e08daeda99be5498777cf7f0d27e.

--- AngularTourOfHeroes/app/hero-detail.component.ts
+++ AngularTourOfHeroes/app/hero-detail.component.ts
@@ -1,12 +1,12 @@
-import {Component, Input} from '@angular/core';
-import { Hero} from './hero';
+import { Component, Input } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'my-hero-detail',
   template: `
   <div *ngIf="hero">
     <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</label></div>
+    <div><label>id: </label>{{hero.id}}</div>
     <div>
       <label>name: </label>
       <input [(ngModel)]="hero.name" placeholder="name"/>
