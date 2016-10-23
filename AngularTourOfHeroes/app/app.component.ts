e667d9a209a3d98a4bbd4d40d4572e9585d4e9b8
Takuya Miyajima <miyajima@acroquest.co.jp>
2016-10-23 22:29:27 +0900

    5章完了

--- AngularTourOfHeroes/app/app.component.ts
+++ AngularTourOfHeroes/app/app.component.ts
@@ -1,22 +1,11 @@
-import { Component} from '@angular/core';
-import { Hero} from './hero';
-
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
+import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: `
+  selector: 'my-app',
+  template: `
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
@@ -30,7 +19,7 @@ const HEROES: Hero[] = [
     <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     `,
 
-    styles: [`
+  styles: [`
   .selected {
     background-color: #CFD8DC !important;
     color: white;
@@ -78,13 +67,24 @@ const HEROES: Hero[] = [
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-`]
+`],
+  providers: [HeroService]
 })
-export class AppComponent {
-    title = 'Tour of Heroes';
-    heroes = HEROES;
-    selectedHero: Hero;
-    onselect(hero: Hero): void {
-        this.selectedHero = hero;
-    }
+export class AppComponent implements OnInit {
+  title = 'Tour of Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
+  constructor(private heroService: HeroService) { }
+  onselect(hero: Hero): void {
+    this.selectedHero = hero;
+  };
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes);
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  };
+
 }
+
