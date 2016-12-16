14e6b25920b2ca796adac5042ba4993e7dffa989
Takuya Miyajima <miyajima@acroquest.co.jp>
2016-12-16 22:52:02 +0900

    6章完了

---
+++ AngularTourOfHeroes/app/heroes.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Router} from '@angular/router';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-heroes',
+  templateUrl: 'heroes.component.html',
+  // providers: [HeroService],
+  styleUrls: ['heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  title = 'Tour of Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
+  constructor(private heroService: HeroService, private router: Router) { }
+  onselect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes);
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+}
+
