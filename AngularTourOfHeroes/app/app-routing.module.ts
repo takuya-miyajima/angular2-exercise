187399018a5609dca65a3d43bce32575d43daa12
Takuya Miyajima <miyajima@acroquest.co.jp>
2016-12-25 21:21:20 +0900

    実装例を参考に、実装違いのあった箇所を是正（最低限）

--- AngularTourOfHeroes/app/app-routing.module.ts
+++ AngularTourOfHeroes/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import {HeroesComponent} from './heroes.component';
 import {HeroDetailComponent} from './hero-detail.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: './dashboard', pathMatch: 'full'},
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'detail/:id', component: HeroDetailComponent},
   {path: 'heroes', component: HeroesComponent}
